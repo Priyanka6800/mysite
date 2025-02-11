51272827e9f828c1f1cd1003f005eecfef21fe92
priy011 <priyanka1@adobe.com>
2025-02-11 10:36:59 +0530

    change

--- blocks/eds-bannerr/eds-bannerr.js
+++
@@ -1,9 +0,0 @@
-function decorateEDSbanner(el) {
-    el.classList.add('decorated');
-  }
-  
-  const banners = document.querySelectorAll('.eds-banner');
-  banners.forEach((el) => {
-    decorateEDSbanner(el);
-  });
-  
