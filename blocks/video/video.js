e86d4f688743df5bd100901f8ee2a467fb2d2ef4
priy011 <priyanka1@adobe.com>
2025-02-12 12:27:11 +0530

    change

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const videoElement = document.querySelector(".video");
     // const fileId = "1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU"; 
-    const videoSrc = 'https://drive.google.com/file/d/1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU/view?usp=drive_link';
+    const videoSrc = 'https://drive.google.com/file/d/1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU/preview';
     
     
     videoElement.src = videoSrc;
