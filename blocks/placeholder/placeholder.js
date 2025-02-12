7f861bab2bc1750e5350053ec5bd7b5aeafb5cb1
priy011 <priyanka1@adobe.com>
2025-02-12 14:28:45 +0530

    update the placeholder webpage

--- blocks/placeholder/placeholder.js
+++ blocks/placeholder/placeholder.js
@@ -1,6 +1,10 @@
-import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js'; 
-export default async function decorate(block)
- { 
-    const placeholders = await fetchPlaceholders(''); 
-    console.log(placeholders);
+import { fetchPlaceholders } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+    const placeholders = await fetchPlaceholders('');
+
+    const preElement = document.createElement('pre');
+    preElement.textContent = JSON.stringify(placeholders, null, 2);
+
+    block.appendChild(preElement);
 }
