df7e8d35a9ce12c75be5c45795194ad85557984b
priy011 <priyanka1@adobe.com>
2025-02-12 12:24:40 +0530

    change

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const videoElement = document.querySelector(".video");
-    const fileId = "1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU"; // Your actual File ID
-    const videoSrc = `https://drive.google.com/uc?export=download&id=${fileId}`;
+    // const fileId = "1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU"; 
+    const videoSrc = 'https://drive.google.com/file/d/1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU/view?usp=drive_link';
+    
     
     videoElement.src = videoSrc;
 });
