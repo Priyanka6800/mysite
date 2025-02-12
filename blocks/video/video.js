eb1f4cbaf48f2b385baec3bbdb0fb3695496c3c9
priy011 <priyanka1@adobe.com>
2025-02-12 12:35:46 +0530

    changes

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,10 +1,15 @@
-function embedGoogleDriveVideo(link) {
-    const embedLink = link;
-    const iframe = document.createElement('iframe');
-    iframe.setAttribute('src', embedLink);
-    const videoDiv = document.querySelector('.video');
-    videoDiv.innerHTML = '';
-    videoDiv.appendChild(iframe);
-}
-const driveLink = 'https://drive.google.com/file/d/1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU/preview';
-embedGoogleDriveVideo(driveLink);
+document.addEventListener("DOMContentLoaded", function() {
+    const videoContainer = document.querySelector(".video"); 
+    const fileId = "1LcbxPiSGiSDzxCabKj9Gssoa0R54-6CU"; 
+    const videoSrc = `https://drive.google.com/file/d/${fileId}/preview`;
+
+   
+    const iframe = document.createElement("iframe");
+    iframe.src = videoSrc;
+    iframe.width = "600"; 
+    iframe.height = "400"; 
+    iframe.allow = "autoplay"; 
+    iframe.frameBorder = "0"; 
+
+    videoContainer.appendChild(iframe);
+});
